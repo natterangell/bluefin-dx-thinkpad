929654e200a9b729ffb1ce1744e411d0dfc411c5
natterangell <35780347+natterangell@users.noreply.github.com>
2024-09-19 18:12:41 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,15 +9,11 @@ RELEASE="$(rpm -E %fedora)"
 
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
+# Install thinkfan, tlp, igt-gpu-tools and displaylink kernel module
+rpm-ostree install tlp tlp-rdw thinkfan igt-gpu-tools /tmp/rpms/kmods/*evdi*.rpm
 
-# this installs a package from fedora repos
-rpm-ostree install screen
+#Disable negativo repo after installing displaylink, as it otherwise conflicts with RPM-fusion
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-multimedia.repo
 
-# this would install a package from rpmfusion
-# rpm-ostree install vlc
-
-#### Example for enabling a System Unit File
-
-systemctl enable podman.socket
+# Mask power-profiles-daemon in order for tlp to work correctly
+systemctl mask power-profiles-daemon.service
