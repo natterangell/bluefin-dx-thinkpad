b27b65086076f7a4e88ab4097b1847a195203b78
natterangell <35780347+natterangell@users.noreply.github.com>
2024-10-04 14:52:23 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -9,12 +9,18 @@ RELEASE="$(rpm -E %fedora)"
 
 # Packages can be installed from any enabled yum repo on the image.
 # RPMfusion repos are available by default in ublue main images
-# Install thinkfan, tlp, igt-gpu-tools and displaylink kernel module
-rpm-ostree override replace --experimental --freeze --from repo='copr:copr.fedorainfracloud.org:natterangell:clevis-tpm1' clevis clevis-luks
-rpm-ostree install tlp thinkfan igt-gpu-tools /tmp/rpms/kmods/*evdi*.rpm
 
-#Disable negativo repo after installing displaylink, as it otherwise conflicts with RPM-fusion
+# Override clevis and clevis-luks from base in favour of own fork with TPM1.2 suport
+rpm-ostree override replace --experimental --freeze --from repo='copr:copr.fedorainfracloud.org:natterangell:clevis-tpm1' clevis clevis-luks
+
+# Install thinkfan, tlp, igt-gpu-tools and displaylink kernel module, as well as (temporary) clevis dependencies
+rpm-ostree install tlp thinkfan igt-gpu-tools /tmp/rpms/kmods/*evdi*.rpm clevis-systemd clevis-dracut
+
+# Disable negativo repo after installing displaylink, as it otherwise conflicts with RPM-fusion
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-multimedia.repo
+
+# Disable own fork, as packages will not be layered
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/natterangell.repo
 
 # Mask power-profiles-daemon in order for tlp to work correctly
 systemctl mask power-profiles-daemon.service
