e95854f2f4d2b6af49909ff65da124da082e69cf
natterangell <35780347+natterangell@users.noreply.github.com>
2024-10-16 14:47:57 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ RELEASE="$(rpm -E %fedora)"
 
 # Install thinkfan, tlp, igt-gpu-tools and displaylink kernel module, as well as (temporary) clevis dependencies
 #rpm-ostree install tlp thinkfan igt-gpu-tools /tmp/rpms/kmods/*evdi*.rpm /tmp/clevis/clevis-dracut-21-1.tpm1u3.fc40.x86_64.rpm /tmp/clevis/clevis-systemd-21-1.tpm1u3.fc40.x86_64.rpm
-rpm-ostree install nvtop /tmp/rpms/kmods/*evdi*.rpm
+rpm-ostree install /tmp/rpms/kmods/*evdi*.rpm
 
 # Disable negativo repo after installing displaylink, as it otherwise conflicts with RPM-fusion
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-multimedia.repo
